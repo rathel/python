49f431c1e6c2a3bf8696e9c0eb4e6436ae9cf758
rathel <rathel@gmail.com>
2019-03-13 10:18:49 -0600

    trying out curses

---
+++ roku_remote/main_curses.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from roku import Roku
+import ssdp
+import os
+import curses
+
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
+
+roku = Roku('192.168.11.103')
+netflix = roku['Netflix']
+plex = roku['Plex']
+primevideo = roku['Prime Video']
+userinput = screen.getstr()
+
+try:
+    while True:
+        char = screen.getch()
+        if char == ord('q'):
+            break
+        elif char == curses.KEY_RIGHT:
+            roku.right()
+        elif char == curses.KEY_LEFT:
+            roku.left()
+        elif char == curses.KEY_UP:
+            roku.up()
+        elif char == curses.KEY_DOWN:
+            roku.down()
+        elif char == curses.KEY_BACKSPACE:
+            roku.back()
+        elif char == 10:
+            roku.select()
+        elif char == ord('p'):
+            roku.play()
+        elif char == ord('i'):
+            curses.echo()
+            userinput
+finally:
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
+
